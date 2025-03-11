72ede2fed4d097d1f6cedee8cbd9a05d3242d6fe
Ламара Ланчава <lmlanch1@mts.ru>
2025-03-11 20:33:15 +0300

    deleteNode

--- linkedList/remove-nth-node-from-end-of-list-19.js
+++ linkedList/remove-nth-node-from-end-of-list-19.js
@@ -1,3 +1,9 @@
+/*
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+*/
+
 function get_node_by_index(node, index) {
     while (index) {
         node = node.next;
