cf7c49172c110853203afaa4a44e610084c486d2
Ламара Ланчава <lmlanch1@mts.ru>
2025-03-24 21:20:06 +0300

    rotate-list-recursion

--- linkedList/constans.mjs
+++ linkedList/constans.mjs
@@ -4,10 +4,9 @@ export function ListNode(val, next) {
 }
 
 
-export const n4 = new ListNode(4);
-export const n3 = new ListNode(3, n4);
-export const n2 = new ListNode(2, n3);
-export const n1 = new ListNode(1, n2);
+export const n3 = new ListNode(2);
+export const n2 = new ListNode(1, n3);
+export const n1 = new ListNode(0, n2);
 
 // export const l6 = new ListNode(1);
 // export const l5 = new ListNode(1, l6);
---
+++ linkedList/rotate-list-61.mjs
@@ -0,0 +1,54 @@
+/*Given the head of a linked list, rotate the list to the right by k places.
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+*/
+
+
+import {ListNode, n1, n2} from "./constans.mjs";
+
+function getLastNode (head) {
+    let current = head
+    while(current.next) {
+        current = current.next
+    }
+    return current
+}
+
+function deleteLastNode(head) {
+    let current = head
+    let nextNode = current.next
+    let prev = null
+    while(current) {
+        if(!nextNode) {
+            prev.next = null
+        }
+        prev = current
+        current = nextNode
+        nextNode = current ? current.next : null
+    }
+    return head
+}
+
+function rotateList(head, k) {
+    if (!k || !head.next) {
+        return head
+    }
+    let current = head
+    let currentNext = current
+    let lastNode = getLastNode(head)
+
+    if(k === 0) {
+        current = lastNode
+        deleteLastNode(currentNext)
+        current.next = currentNext
+        return current
+    }
+    if(k > 0) {
+        current = lastNode
+        deleteLastNode(currentNext)
+        current.next = currentNext
+        return rotateList(current, k-1)
+    }
+}
+
+console.log(rotateList(n1, 2))
