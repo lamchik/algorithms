8ebc5c262a88714849be774f9ae89cbe08a03d2a
Ламара Ланчава <lmlanch1@mts.ru>
2025-03-28 13:44:31 +0300

    remove-duplicates-from-sorted-list-ii-82

--- linkedList/constans.mjs
+++ linkedList/constans.mjs
@@ -8,9 +8,9 @@ export const n3 = new ListNode(2);
 export const n2 = new ListNode(1, n3);
 export const n1 = new ListNode(0, n2);
 
-// export const l6 = new ListNode(1);
-// export const l5 = new ListNode(1, l6);
-// export const l4 = new ListNode(1, l5);
-export const l3 = new ListNode(1);
-export const l2 = new ListNode(0, l3);
-export const l1 = new ListNode(1, l2);
+export const l6 = new ListNode(2);
+export const l5 = new ListNode(0, l6);
+export const l4 = new ListNode(0, l5);
+export const l3 = new ListNode(-1, l4);
+export const l2 = new ListNode(-1, l3);
+export const l1 = new ListNode(-3, l2);
